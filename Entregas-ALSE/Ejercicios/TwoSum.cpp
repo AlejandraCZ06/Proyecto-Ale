50ea6d3fcd9550218f857e5b768fb8140fbdc490
ALe <alejandra.lea.2023@gmail.com>
2025-03-11 20:07:21 -0500

    cambios

--- Entregas-ALSE/Ejercicios/TwoSum.cpp
+++ Entregas-ALSE/Ejercicios/TwoSum.cpp
@@ -1,28 +1,34 @@
+#include <iostream>
 #include <vector>
 #include <unordered_map>
 
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        // Crear un mapa hash para almacenar los números y sus índices
         std::unordered_map<int, int> numMap;
         
-        // Iterar sobre el vector
         for (int i = 0; i < nums.size(); ++i) {
-            // Calcular el complemento necesario para alcanzar el target
             int complement = target - nums[i];
             
-            // Verificar si el complemento ya está en el mapa
             if (numMap.find(complement) != numMap.end()) {
-                // Si se encuentra, devolver los índices
                 return {numMap[complement], i};
             }
             
-            // Si no se encuentra, agregar el número actual y su índice al mapa
             numMap[nums[i]] = i;
         }
         
-        // Devolver un vector vacío si no se encuentra solución (aunque se garantiza una solución)
         return {};
     }
 };
+
+int main() {
+    Solution solution;
+    std::vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    
+    std::vector<int> result = solution.twoSum(nums, target);
+    
+    std::cout << "Output: [" << result[0] << ", " << result[1] << "]" << std::endl;
+    
+    return 0;
+}
