44da3c93c1bef46ac0f90bbe7096d4451091114f
ALe <alejandra.lea.2023@gmail.com>
2025-03-22 23:52:39 -0500

    CAMBNIOS

---
+++ Entregas-ALSE/Ejercicios/36.ValidSudoku.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; ++i) {
+            unordered_set<char> rows, cols, cube;
+            for (int j = 0; j < 9; ++j) {
+                // Check row
+                if (board[i][j] != '.' && !rows.insert(board[i][j]).second) {
+                    return false;
+                }
+                // Check column
+                if (board[j][i] != '.' && !cols.insert(board[j][i]).second) {
+                    return false;
+                }
+                // Check 3x3 sub-box
+                int rowIndex = 3 * (i / 3) + j / 3;
+                int colIndex = 3 * (i % 3) + j % 3;
+                if (board[rowIndex][colIndex] != '.' && !cube.insert(board[rowIndex][colIndex]).second) {
+                    return false;
+                }
+            }
+        }
+        return true; // Ensure a return value is provided
+    }
+};
+
+int main() {
+    vector<vector<char>> board(9, vector<char>(9));
+    string input;
+    cout << "Ingrese el tablero de Sudoku:" << endl;
+
+    for (int i = 0; i < 9; ++i) {
+        getline(cin, input);
+        int k = 0;
+        for (char c : input) {
+            if (c != ',' && c != '[' && c != ']') {
+                board[i][k++] = c;
+            }
+        }
+    }
+
+    Solution solution;
+    if (solution.isValidSudoku(board)) {
+        cout << "Output: true" << endl;
+    } else {
+        cout << "Output: false" << endl;
+    }
+
+    return 0;
+}
