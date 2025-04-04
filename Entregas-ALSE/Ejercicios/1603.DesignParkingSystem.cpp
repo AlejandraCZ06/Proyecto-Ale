7582618c81833b1f2cc3cf2a88f58e8d1a6de569
ALe <alejandra.lea.2023@gmail.com>
2025-04-03 20:08:48 -0500

    cambios

--- Entregas-ALSE/Ejercicios/1603.DesignParkingSystem.cpp
+++ Entregas-ALSE/Ejercicios/1603.DesignParkingSystem.cpp
@@ -1,117 +1,51 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <sstream>
-using namespace std;
-
-// Clase que representa el sistema de estacionamiento
+// Clase que gestiona un estacionamiento con espacios de distintos tamaños
 class ParkingSystem {
-private:
-    int bigSlots;    // Espacios disponibles para coches grandes
-    int mediumSlots; // Espacios disponibles para coches medianos
-    int smallSlots;  // Espacios disponibles para coches pequeños
-
-public:
-    // Constructor que inicializa los espacios para cada tipo de coche
-    ParkingSystem(int big, int medium, int small) {
-        bigSlots = big;
-        mediumSlots = medium;
-        smallSlots = small;
-    }
+    private:
+        // Variables que almacenan la cantidad de espacios disponibles para cada tipo de vehículo
+        int bigSlots;      // Espacios asignados a autos grandes (tipo 1)
+        int mediumSlots;   // Espacios asignados a autos medianos (tipo 2)
+        int smallSlots;    // Espacios asignados a autos pequeños (tipo 3)
     
-    // Método para agregar un coche al estacionamiento
-    bool addCar(int carType) {
-        if (carType == 1) { // Coche grande
-            if (bigSlots > 0) {
-                bigSlots--; // Decrementa el contador de espacios grandes
-                return true;
-            }
-        } else if (carType == 2) { // Coche mediano
-            if (mediumSlots > 0) {
-                mediumSlots--; // Decrementa el contador de espacios medianos
-                return true;
-            }
-        } else if (carType == 3) { // Coche pequeño
-            if (smallSlots > 0) {
-                smallSlots--; // Decrementa el contador de espacios pequeños
-                return true;
-            }
+    public:
+        // Constructor que inicializa el estacionamiento con la cantidad de espacios disponibles de cada tipo
+        ParkingSystem(int big, int medium, int small) {
+            bigSlots = big;
+            mediumSlots = medium;
+            smallSlots = small;
         }
-        return false; // No hay espacio disponible para el tipo de coche
-    }
-};
-
-int main() {
-    string commandsInput, dataInput;
+        
+        // Método que intenta estacionar un auto según su tipo
+        // Retorna true si el auto pudo ser estacionado, false si no hay espacio disponible
+        bool addCar(int carType) {
+            // Variable que indica si se encontró un espacio para el auto
+            bool hayEspacio = false;
+            
+            // Verifica si hay espacio disponible para un auto grande (tipo 1)
+            if (carType == 1) {
+                if (bigSlots > 0) { // Si hay al menos un espacio libre
+                    bigSlots--; // Se ocupa un espacio
+                    hayEspacio = true; // Se confirma que el auto pudo ser estacionado
+                }
+            }
+            
+            // Verifica si hay espacio disponible para un auto mediano (tipo 2)
+            if (carType == 2) {
+                if (mediumSlots > 0) { // Si hay al menos un espacio libre
+                    mediumSlots--; // Se ocupa un espacio
+                    hayEspacio = true; // Se confirma que el auto pudo ser estacionado
+                }
+            }
+            
+            // Verifica si hay espacio disponible para un auto pequeño (tipo 3)
+            if (carType == 3) {
+                if (smallSlots > 0) { // Si hay al menos un espacio libre
+                    smallSlots--; // Se ocupa un espacio
+                    hayEspacio = true; // Se confirma que el auto pudo ser estacionado
+                }
+            }
+            
+            // Retorna true si el auto pudo ser estacionado, de lo contrario, retorna false
+            return hayEspacio;
+        }
+    };
     
-    // Solicitar al usuario que ingrese los comandos
-    cout << "Ingrese los comandos: ";
-    getline(cin, commandsInput);
-    
-    // Solicitar al usuario que ingrese los datos
-    cout << "Ingrese los datos: ";
-    getline(cin, dataInput);
-
-    // Procesar los datos de entrada
-    vector<string> commands;
-    vector<vector<int>> inputs;
-    stringstream ssCommands(commandsInput);
-    stringstream ssData(dataInput);
-    string command;
-    char ch;
-
-    // Leer comandos del input
-    while (ssCommands >> ch) {
-        if (ch == '"') {
-            getline(ssCommands, command, '"');
-            commands.push_back(command);
-        }
-    }
-
-    // Leer datos del input
-    vector<int> temp;
-    int num;
-    while (ssData >> ch) {
-        if (isdigit(ch)) {
-            ssData.putback(ch);
-            ssData >> num;
-            temp.push_back(num);
-        } else if (ch == ']') {
-            if (!temp.empty()) {
-                inputs.push_back(temp);
-                temp.clear();
-            }
-        }
-    }
-
-    vector<string> output;
-    ParkingSystem* parkingSystem = nullptr;
-
-    // Procesar cada comando
-    for (size_t i = 0; i < commands.size(); ++i) {
-        if (commands[i] == "ParkingSystem") {
-            // Inicializar el sistema de estacionamiento
-            parkingSystem = new ParkingSystem(inputs[i][0], inputs[i][1], inputs[i][2]);
-            output.push_back("null");
-        } else if (commands[i] == "addCar") {
-            // Intentar agregar un coche y almacenar el resultado
-            bool result = parkingSystem->addCar(inputs[i][0]);
-            output.push_back(result ? "true" : "false");
-        }
-    }
-
-    // Imprimir la salida
-    cout << "Respuesta: [";
-    for (size_t i = 0; i < output.size(); ++i) {
-        cout << output[i];
-        if (i < output.size() - 1) {
-            cout << ",";
-        }
-    }
-    cout << "]" << endl;
-
-    // Liberar la memoria del objeto ParkingSystem
-    delete parkingSystem;
-
-    return 0;
-}
