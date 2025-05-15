98d896d19b84869f3352945c7949bb361bbcf6b5
ALe <alejandra.lea.2023@gmail.com>
2025-05-15 15:26:47 -0500

    cmake

---
+++ Entregas-ALSE/proyecto_op3_2025/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/almeja/Ejemplo_1/AlejandraCZ06/Proyecto-Ale/Entregas-ALSE/proyecto_op3_2025
+# Build directory: /home/almeja/Ejemplo_1/AlejandraCZ06/Proyecto-Ale/Entregas-ALSE/proyecto_op3_2025/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
