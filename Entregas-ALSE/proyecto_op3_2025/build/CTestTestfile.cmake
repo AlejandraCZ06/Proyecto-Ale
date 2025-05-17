85dbe28a23cf303bdda3d34c361f2d80823a2307
ANHM323 <herreraandy3321@outlook.com>
2025-05-17 18:50:25 -0500

    make over

--- Entregas-ALSE/proyecto_op3_2025/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/almeja/Ejemplo_1/AlejandraCZ06/Proyecto-Ale/Entregas-ALSE/proyecto_op3_2025
-# Build directory: /home/almeja/Ejemplo_1/AlejandraCZ06/Proyecto-Ale/Entregas-ALSE/proyecto_op3_2025/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
