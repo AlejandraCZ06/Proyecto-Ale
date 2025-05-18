78a2fceaf7fde25522a416e94e73c47c4d0f22e6
ANHM323 <herreraandy3321@outlook.com>
2025-05-18 16:58:14 -0500

    funciona

--- Entregas-ALSE/proyecto_op3_2025/build-Gestor_de_Inventario-Desktop-Debug/Gestor_de_Inventario_autogen/include/ui_mainwindow.h
+++ Entregas-ALSE/proyecto_op3_2025/build-Gestor_de_Inventario-Desktop-Debug/Gestor_de_Inventario_autogen/include/ui_mainwindow.h
@@ -16,6 +16,7 @@
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QScrollBar>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTableWidget>
 #include <QtWidgets/QWidget>
@@ -27,31 +28,40 @@ class Ui_MainWindow
 public:
     QWidget *centralwidget;
     QTableWidget *tableWidget;
+    QScrollBar *verticalScrollBar;
     QMenuBar *menubar;
     QMenu *menuinventario;
+    QMenu *menuNuevo_item;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(800, 600);
+        MainWindow->resize(814, 525);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         tableWidget = new QTableWidget(centralwidget);
         tableWidget->setObjectName(QString::fromUtf8("tableWidget"));
-        tableWidget->setGeometry(QRect(5, -9, 651, 361));
+        tableWidget->setGeometry(QRect(80, 100, 611, 301));
+        verticalScrollBar = new QScrollBar(centralwidget);
+        verticalScrollBar->setObjectName(QString::fromUtf8("verticalScrollBar"));
+        verticalScrollBar->setGeometry(QRect(690, 99, 16, 301));
+        verticalScrollBar->setOrientation(Qt::Vertical);
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 800, 19));
+        menubar->setGeometry(QRect(0, 0, 814, 19));
         menuinventario = new QMenu(menubar);
         menuinventario->setObjectName(QString::fromUtf8("menuinventario"));
+        menuNuevo_item = new QMenu(menubar);
+        menuNuevo_item->setObjectName(QString::fromUtf8("menuNuevo_item"));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
         MainWindow->setStatusBar(statusbar);
 
+        menubar->addAction(menuNuevo_item->menuAction());
         menubar->addAction(menuinventario->menuAction());
 
         retranslateUi(MainWindow);
@@ -63,6 +73,7 @@ public:
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
         menuinventario->setTitle(QCoreApplication::translate("MainWindow", "inventario", nullptr));
+        menuNuevo_item->setTitle(QCoreApplication::translate("MainWindow", "Nuevo item", nullptr));
     } // retranslateUi
 
 };
